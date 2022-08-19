6f1deab3c238389e51a821587d042a3ea015d041
ashley <ashley.oketo@student.moringaschool.com>
2022-08-19 23:29:38 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const element = document.getElementById("main");
+element.remove(main);
+
+const newHeader = document.createElement('h1');
+
+
+newHeader.id = 'victory';
+ 
+newHeader.innerHTML = "YOUR-NAME is the champion";
+
